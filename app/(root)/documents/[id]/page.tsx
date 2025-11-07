4ae5a51a42476539f1c23f71bd69e6ada142f895
Alfin123-stack <alfinkarin789@gmail.com>
2025-11-07 11:06:25 +0700

    add editor jsm and install clerk

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { Editor } from "@/components/editor/Editor";
+import { Button } from "@/components/ui/button";
+import { User } from "lucide-react";
+import React from "react";
 
 function page() {
   return (
-    <div>page</div>
-  )
+    <div className="px-14 py-2 bg-gray-600">
+      <header className="p-4">
+        <nav className=" flex justify-between items-center">
+          <h1>live docs</h1>
+          <p>title</p>
+          <div className="flex justify-items-center items-center gap-4">
+            <Button>Share</Button>
+            <User />
+          </div>
+        </nav>
+      </header>
+      <Editor />
+    </div>
+  );
 }
 
-export default page
+export default page;
